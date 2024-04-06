d6877087c8bd181c8538d8c6f5370ae947a0ea13
meghnaaxx <109579421+meghnaaxx@users.noreply.github.com>
2024-04-06 16:49:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,62 +1,47 @@
 var amount = document.getElementById('amount');
 var currency = 'USD';
-var ratep = 0.0118;
+
+var dict = {
+"EUR":92.98,
+"USD":84.8,
+"GBP":106.47,
+"CAD":62.04,
+"AUD":55.57,
+"DKK":12.65,
+"HKD":10.69,
+"JPY":1.1,
+"NZD":50.75,
+"QAR":23.07,
+"RUB":1.4,
+"SAR":22.37
+}
 
 amount.addEventListener('input', function() {
     update();
 });
 
-function rate_update() {
-  run('INR', currency)
-        .then(rate => {
-          console.log(rate);
-          let val = (rate).toFixed(5);
-            document.getElementById('rate').innerHTML = '1 INR = ' +  val + ' ' + currency;
-            ratep = (rate).toFixed(5);
-        })
-        .then(rate=>{
-          console.log(rate);
-          update();
-        })
-        .catch(error => {
-            console.error('Error occurred:', error);
-        });
+function toggle() {
+  var optionsDiv = document.getElementById("options");
+  if (optionsDiv.style.display === "none") {
+      optionsDiv.style.display = "block";
+  } else {
+      optionsDiv.style.display = "none";
+  }
 }
 
-function run(from, to) {
-    const endpoint = 'convert';
-    const access_key = '926d1be6e8159f8b0203c8dcb5c2f65e';
-    const amount = '1';
-    
-    // Construct the URL
-    const url = `https://api.currencylayer.com/${endpoint}?access_key=${access_key}&from=${from}&to=${to}&amount=${amount}`;
-    
-    // Execute the conversion using Fetch API
-    return fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        let valuex = document.getElementById('amount').value;
-        valuex = valuex + 1750;
-        let ratepx = data.result;
-        document.getElementById('rquantity').value = (valuex/ratepx).toFixed(3);
-        return data.result;
-      })
-      .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-        throw error;
-      });
+function selectOption(currency_n) { 
+  currency = currency_n;
+  update();
+  document.getElementById("selectedCurrency").innerHTML = currency_n + ' <button id="toggleOptionsButton" onclick="toggle()">↓</button>';
+  document.getElementById("options").style.display = "none";
 }
 
 
 
 function update() {
     let value = amount.value;
-    value = value/ratep;
+    let rate = dict[currency];
+    value = value*rate;
     
     let gst;
 
@@ -67,17 +52,18 @@ function update() {
     } else {
         gst = Math.min(5500 + (value - 1000000) * 0.001, 60000);
     }
-    gst=gst*0.1802;
+    gst=gst*0.18;
     
     value = value+gst;
     value = value+1750;
-    value = value*1.00238;
+    value = value;
     let x = (gst + 1750).toFixed(0);
-    console.log(value);
-    document.getElementById('totalfee').innerHTML = x + ' INR';
 
+    document.getElementById('totalfee').innerHTML = x + ' INR';
+    document.getElementById('rate').innerHTML = '1 INR = ' + (1/rate).toFixed(4) + ' ' + currency;
     document.getElementById('convertamount').innerHTML = gst.toFixed(0) + ' INR';
-    document.getElementById('rquantity').value = (value).toFixed(2);
+    document.getElementById('rquantity').value = (value).toFixed(1);
+
     if(value>=700000){
       document.getElementById('notice').style.display = 'block';
     } else {
@@ -94,6 +80,7 @@ recieved_quantity.addEventListener('input', function() {
 });
 
 function update_rev() {
+    let rate = dict[currency];
     var val = parseFloat(recieved_quantity.value);
     if(val>=700000){
       document.getElementById('notice').style.display = 'block';
@@ -109,30 +96,15 @@ function update_rev() {
     } else {
         gst = Math.min(5500 + (val - 1000000) * 0.001, 60000);
     }
-    gst = gst*0.1803;
+    gst = gst*0.18;
     val = val-gst;
     val = val - 1750;
     let x = (gst + 1750).toFixed(0);
+
     document.getElementById('totalfee').innerHTML = x + ' INR';
-
-
+    document.getElementById('rate').innerHTML = '1 INR = ' + (1/rate).toFixed(4) + ' ' + currency;
     document.getElementById('convertamount').innerHTML = gst.toFixed(0) + ' INR';
-    document.getElementById('amount').value = (val*ratep).toFixed(2);
+    document.getElementById('amount').value = (val/rate).toFixed(1);
 
 }
 
-function toggle() {
-  var optionsDiv = document.getElementById("options");
-  if (optionsDiv.style.display === "none") {
-      optionsDiv.style.display = "block";
-  } else {
-      optionsDiv.style.display = "none";
-  }
-}
-
-function selectOption(currency_n) { 
-  currency = currency_n;
-  rate_update();
-  document.getElementById("selectedCurrency").innerHTML = currency_n + ' <button id="toggleOptionsButton" onclick="toggle()">↓</button>';
-  document.getElementById("options").style.display = "none";
-}
